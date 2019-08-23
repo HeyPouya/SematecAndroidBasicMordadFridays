69fb3aa03b3788145b1c8678ef0f91ef733e29df
Pouya <pouyaheydary@gmail.com>
2019-08-23 14:35:26 +0430

    end of session 2

--- app/src/main/java/com/sematec/sematecandroidbasicmordadfridays/MainActivity.java
+++ app/src/main/java/com/sematec/sematecandroidbasicmordadfridays/MainActivity.java
@@ -1,8 +1,9 @@
 package com.sematec.sematecandroidbasicmordadfridays;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("MYTAG","We are in onCreate method");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MYTAG","We are in onStart method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYTAG","We are in onResume method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
